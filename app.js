2201db0e9c15a8378b3afabceb3508e974b4f20d
Chaithanyareddypailla <chaithanyareddypailla@gmail.com>
2025-04-01 17:04:36 -0500

    added files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var chimesRouter = require('./routes/chimes');
+var gridRouter = require('./routes/grid');
 
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/chimes', chimesRouter);
+app.use('/grid', gridRouter);
 
 
 
